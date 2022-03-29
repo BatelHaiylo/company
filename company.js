fd29c881d42a342141632f66e33ccbb1532c1a37
Batel Haiylo <inpo686@gmail.com>
2022-03-29 21:36:54 +0300

    company

---
+++ company.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myCheck");
+    x.checked = true;
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myEmail").value;
+    document.getElementById("demo").innerHTML = x;
+  }
